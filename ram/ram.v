4d3c4feea26e9f09e64619c2bbf56d066891065a
WimukthiPWijerathne <wimukthiwijerathne@gmail.com>
2025-04-11 20:01:50 +0530

    did some changes

--- ram/ram.v
+++ ram/ram.v
@@ -1,6 +1,7 @@
 module ram1(addr,data,clk,rd,wr,cs);
     input [9:0] addr; //declare a10 bit input adder
-    input [7:0] data; //declare a 8 bit input data
+    inout [7:0] data;
+    inout clk,rd,wr,cs; //declare a 8 bit input data
     reg [7:0] mem[1023:0]; //declare a 8 bit memory of 1024 words
     reg [7:0] d_out; //declare a 8 bit output data
 
